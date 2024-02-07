b4e37c59bc7c2fa64a01e63ec0d49a08eb968f8d
nikich8 <nikich8@gmail.com>
2024-02-07 22:42:09 +0530

    roboshop shell script

--- redis.sh
+++ redis.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-exec &>LOGFILE
+#exec &>LOGFILE #here logs r not run in terminal.it will run in background
 
 echo "script started exicuting at $TIMESTAMP" &>> $LOGFILE
 
@@ -33,20 +33,20 @@ else
     echo "you are root user"
 fi
 
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOGFILE
 VALIDATE $? "Installing Remi release"
 
-dnf module enable redis:remi-6.2 -y
+dnf module enable redis:remi-6.2 -y &>> $LOGFILE
 VALIDATE $? "enabling redis"
 
-dnf install redis -y
+dnf install redis -y &>> $LOGFILE
 VALIDATE $? "Installing redis"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis/.conf
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis/.conf &>> $LOGFILE
 VALIDATE $? "allowing remote connections"
-
-systemctl enable redis
+ 
+systemctl enable redis &>> $LOGFILE
 VALIDATE $? "Enabled redis"
 
-systemctl start redis
+systemctl start redis &>> $LOGFILE
 VALIDATE $? "Started redis"
